11602277a7e295cde60e1ec018df1bc6a6d2e86a
Téo <teo@MacBook-Pro-de-Teo.local>
2025-01-14 19:21:23 +0100

    Creation of project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Hero from "@/components/Hero";
+export default function Home() {
+  return (
+<main className="relative bg-background-dark text-foreground-dark">
+
+<Hero />
+</main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,51 @@
+import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background-light": "var(--background-light)",
+        "foreground-light": "var(--foreground-light)",
+        "background-dark": "var(--background-dark)",
+        "foreground-dark": "var(--foreground-dark)",
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  darkMode: "class",
+
+  plugins: [addVariablesForColors],
+  
+} satisfies Config;
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
